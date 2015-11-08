f429be845623ef128f13880042e4000f3c03286f
Olivier Andriessen <olivierandriessen@gmail.com>
2015-11-08 11:45:19 +0100

    Comform bootstrap modal

--- build/angular-ui-components.js
+++ build/angular-ui-components.js
@@ -4,8 +4,8 @@ angular
     .module('angular-ui-components')
     .run(function($templateCache) {
 
-        $templateCache.put('templates/ui/ui-modal.html', '<div class="ui-modal"><div class="ui-modal-container"><div class="ui-modal-dialog"><div class="ui-modal-content"><ng-transclude></ng-transclude></div></div></div></div>');
-        $templateCache.put('templates/ui/ui-popover.html', '<div class="ui-popover"><div class="ui-popover-inner"><ng-transclude></ng-transclude></div></div>');
+        $templateCache.put('templates/ui/ui-modal.html', '<div class="modal"><div class="modal-container"><div class="modal-dialog"><div class="modal-content"><ng-transclude></ng-transclude></div></div></div></div>');
+        $templateCache.put('templates/ui/ui-popover.html', '<div class="popover"><div class="popover-inner"><ng-transclude></ng-transclude></div></div>');
 
     });
 ;angular
